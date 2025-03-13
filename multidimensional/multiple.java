cc71b29749f779706ffce78917bde39ac7666642
Adhithyan <adhithyan26adhi@gmail.com>
2025-03-13 13:54:41 +0530

    program

---
+++ multidimensional/multiple.java
@@ -0,0 +1,10 @@
+public class Multiple{
+    public static void main(String[] args) {
+        int numbera[][]={10,20},{30,40}}
+        System.out.println(a[0][0]);
+        System.out.println(a[0][1]);
+        System.out.println(a[1][0]);
+        System.out.println(a[1][1]);
+    }
+}
+
