c38f4213f7bb1d348d2e6b6ef2948bcaec960f16
Adhithyan <adhithyan26adhi@gmail.com>
2025-03-13 11:52:52 +0530

    program

---
+++ commands/statement.java
@@ -0,0 +1,11 @@
+public class statement{
+    public static void main(String[]args){
+    if(true){
+        System.out.println(true);
+    }
+     if(false){
+         System.out.println(false);
+
+        }
+        }
+    }
