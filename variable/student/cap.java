0f4517dfa6f48278131e8bbd23bf870f622b1d3d
Adhithyan <adhithyan26adhi@gmail.com>
2025-03-13 11:40:07 +0530

    program

---
+++ variable/student/cap.java
@@ -0,0 +1,8 @@
+public class cap{
+    public static void main(String args[]){
+        int num = 20;
+        int num1=40;
+        System.out.println(num);
+        System.out.println(num1);
+    }
+}
