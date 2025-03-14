21ce6b3276e54cbc893b13c3336549ba3fd56d27
Adhithyan <adhithyan26adhi@gmail.com>
2025-03-14 12:08:32 +0530

    program

---
+++ cases/Case.java
@@ -0,0 +1,7 @@
+public class Case{
+    public static void main(String[] args){
+        String name="hello,world";
+        System.out.print(name.toLowerCase());
+        System.out.print(name.toUpperCase());
+    }
+}
