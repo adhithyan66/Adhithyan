b80cbb009c51e7ecc482e1dcec205210063d9ede
Adhithyan <adhithyan26adhi@gmail.com>
2025-03-14 12:15:55 +0530

    program

--- table/Table.java
+++ table/Table.java
@@ -1,5 +1,6 @@
 public class Table{
-    public static void main(String[]args) {
+    public static 
+void main(String[]args) {
         int i=1;
      while(i<10){
            System.out.println(i+"x7="+1*7);
--- vowel/Vowel.java
+++ vowel/Vowel.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
-
 public class Vowel{
     public static void main(String[]args) {
-        Scanner scanner = new Scanner(System.in);
-     System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
-        int vowelCount = 0;
-        String vowels = "aeiouAEIOU";
-        for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-            if (vowels.indexOf(ch) != -1) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a string: ");
+    String input = scanner.nextLine();
+    int vowelCount =0;
+    String vowels ="aeiouAEIOU";
+    for (int i = 0; i < input.length(); i++) {
+    char ch = input.charAt(i);
+       if (vowels.indexOf(ch) != -1){
                 vowelCount++;
             }
         }
