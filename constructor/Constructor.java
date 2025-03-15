dbe787bb2c5755a6fbfd00c816faa993fd95775d
Adhithyan <adhithyan26adhi@gmail.com>
2025-03-15 10:41:54 +0530

    program

---
+++ constructor/Constructor.java
@@ -0,0 +1,9 @@
+public class Constructor {
+    Constructor(){
+   System.out.println("welcome");
+    }
+    public static void main(String[] args) {
+        Constructor obj =new Constructor();
+        } 
+    }
+
