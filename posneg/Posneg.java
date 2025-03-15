64cd3d17d732ec29e16102f6d1ee9029b5340407
Adhithyan <adhithyan26adhi@gmail.com>
2025-03-15 10:20:18 +0530

    program

---
+++ posneg/Posneg.java
@@ -0,0 +1,12 @@
+public class Posneg{
+    int a;
+        public static void main(String[] args) {
+        int a=20;
+        if(a>1){
+     System.out.println("a is positive.");
+        } 
+        else {
+            System.out.println("a is negative.");
+        }
+    }
+}
