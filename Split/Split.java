95d904f08dee4c2a21a52c1b444c69d4d8dd419d
Adhithyan <adhithyan26adhi@gmail.com>
2025-03-15 10:32:37 +0530

    program

---
+++ Split/Split.java
@@ -0,0 +1,7 @@
+public class Split {
+    public static void main(String[] args) {
+        String name = "hello,World";
+        String[] names = name.split(","); 
+               System.out.println(names[0]);
+    }
+}
